bf82f01fb1c2748783d9ebb779abe97c3008a6c4
purnimasingh123 <purnima7791@gmail.com>
2020-02-19 10:53:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MegaMenuDemo from './components/megaMenuDemo';
+import Diary from './components/Diary';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MegaMenuDemo/>
+      <Diary/>
+    </div>
+  );
+}
+
+export default App;
